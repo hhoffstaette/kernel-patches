e99bb71ab0f79ce8d830db1673b2621013f9a282
Holger Hoffstätte <holger@applied-asynchrony.com>
2017-07-12 18:26:15 +0200

    4.9: improve apply.sh with --help and --force

--- apply.sh
+++ apply.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
+
+# use consistent options for patch
 PATCHOPTS="-F0 -p1 -s"
+
+# help
+if [[ $1 == "-h" ]] || [[ $1 == "--help" ]] ; then
+	echo
+	echo "Usage: " $0 "[OPTION]"
+	echo
+	echo "Options are:"
+	echo "  -f, --force : do not verify kernel release against patch series tag"
+	echo "  -h, --help  : this message"
+	echo
+	exit 0
+fi
 
 # make sure $PWD is a kernel tree
 [[ ! -f Kbuild ]] && echo "Error: no kernel tree found in current directory." && exit 1
@@ -19,10 +33,12 @@ PATCHDIR=$(dirname $0)/${VERSION}.${PATCHLEVEL}
 	&& exit 1
 
 # check release version against patch series tag
-RELEASE=$(git -C $(dirname $0) tag --list --sort=version:refname | tail -n 1)
-[[ ${RELEASE} != ${VERSION}.${PATCHLEVEL}.${SUBLEVEL} ]] \
-	&& echo "Error: patchset does not match kernel release" ${VERSION}.${PATCHLEVEL}.${SUBLEVEL} \
-	&& exit 1
+if [[ $1 != "-f" ]] && [[ $1 != "--force" ]] ; then
+	RELEASE=$(git -C $(dirname $0) tag --list --sort=version:refname | tail -n 1)
+	[[ ${RELEASE} != ${VERSION}.${PATCHLEVEL}.${SUBLEVEL} ]] \
+		&& echo "Error: patchset does not match kernel release" ${VERSION}.${PATCHLEVEL}.${SUBLEVEL} \
+		&& exit 1
+fi
 
 # find the list of patches to apply
 PATCHES=$(\ls -1 ${PATCHDIR}/*.patch)
