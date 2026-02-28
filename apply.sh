a1a559f2def2693d0d404f5a9d3c01cc6200c0c6
Holger Hoffstätte <holger@applied-asynchrony.com>
2026-02-28 11:52:22 +0100

    6.18: fix git tag scraping even more
    Apparently tags are not stored (or retrieved?) in strict chronological
    tagging order, so we need to get all tags for a commit, sort them
    properly (to accommodate .9 <-> .10 ordering) and then get only the
    last tag.

--- apply.sh
+++ apply.sh
@@ -31,7 +31,7 @@ PATCHDIR=$(dirname $0)/${VERSION}.${PATCHLEVEL}
 
 # check release version against patch series tag
 if [[ $1 != "-f" ]] && [[ $1 != "--force" ]] ; then
-	RELEASE=$(git -C $(dirname $0) describe --exact-match --tags)
+	RELEASE=$(git -C $(dirname $0) tag --points-at | sort --version-sort | tail -n 1)
 	[[ ${RELEASE} != ${VERSION}.${PATCHLEVEL}.${SUBLEVEL} ]] \
 		&& echo "Error: patchset ${RELEASE} does not match kernel release" ${VERSION}.${PATCHLEVEL}.${SUBLEVEL} \
 		&& exit 1
